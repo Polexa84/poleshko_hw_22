3c6e87d445a3b126a5e9d81dc34a4b62228879ff
polexa84 <polexa84@gmail.com>
2025-04-06 18:44:25 +0300

    Добавление приложения “blog” в настройки исправлена ошибка

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',  # <-- Добавьте свое приложения
     'crispy_forms', # <-- Добавлена работа с формами
-    'catalog', # <-- Добавьте свое приложения
+    'blog', # <-- Добавьте свое приложения
 ]
 
 MIDDLEWARE = [
