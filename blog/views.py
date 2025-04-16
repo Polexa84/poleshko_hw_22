0fe7257f19a7f46682ff031086b1a13b4f310894
polexa84 <polexa84@gmail.com>
2025-04-16 21:18:44 +0300

    доработана админка для назначения и удаления групп для корректной работы

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import BlogPost  # Импортируем модель BlogPost
 from .forms import PostForm  # Импортируем форму PostForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin  # Импортируем PermissionRequiredMixin
 
 class BlogListView(ListView):
     """
@@ -27,6 +28,12 @@ class BlogDetailView(DetailView):
     template_name = 'blog/blog_detail.html'  # Указываем шаблон для отображения
     context_object_name = 'blog_post'  # Имя переменной в контексте, содержащей информацию о посте
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
@@ -35,7 +42,7 @@ class BlogDetailView(DetailView):
         context['can_publish'] = user.has_perm('blog.can_publish_blogpost')
         return context
 
-class BlogCreateView(CreateView):
+class BlogCreateView(PermissionRequiredMixin, CreateView):
     """
     Представление для создания нового поста блога.
     """
@@ -43,8 +50,13 @@ class BlogCreateView(CreateView):
     form_class = PostForm  # Используем форму PostForm
     template_name = 'blog/blog_form.html'  # Указываем шаблон для отображения
     success_url = reverse_lazy('blog:blog_list')  # URL для перенаправления после успешного создания
+    permission_required = 'blog.add_blogpost'  # Требуемое разрешение
 
-class BlogUpdateView(UpdateView):
+    def handle_no_permission(self):
+        from django.shortcuts import redirect
+        return redirect('blog:blog_list') # Или любая другая страница
+
+class BlogUpdateView(PermissionRequiredMixin, UpdateView):
     """
     Представление для редактирования поста блога.
     """
@@ -52,8 +64,13 @@ class BlogUpdateView(UpdateView):
     form_class = PostForm  # Используем форму PostForm
     template_name = 'blog/blog_form.html'  # Указываем шаблон для отображения
     context_object_name = 'blog_post'
+    permission_required = 'blog.change_blogpost'  # Требуемое разрешение
 
-class BlogDeleteView(DeleteView):
+    def handle_no_permission(self):
+        from django.shortcuts import redirect
+        return redirect('blog:blog_list') # Или любая другая страница
+
+class BlogDeleteView(PermissionRequiredMixin, DeleteView):
     """
     Представление для удаления поста блога.
     """
@@ -61,3 +78,8 @@ class BlogDeleteView(DeleteView):
     template_name = 'blog/blog_confirm_delete.html'  # Указываем шаблон для отображения
     success_url = reverse_lazy('blog:blog_list')  # URL для перенаправления после успешного удаления
     context_object_name = 'blog_post'
+    permission_required = 'blog.delete_blogpost'  # Требуемое разрешение
+
+    def handle_no_permission(self):
+        from django.shortcuts import redirect
+        return redirect('blog:blog_list')
